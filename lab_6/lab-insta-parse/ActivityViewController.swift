a48bb68d222efb9a65be31d6834b41026a2b96dd
Jayleen Espinal <122298473+JayleenEspinal@users.noreply.github.com>
2024-04-14 23:38:39 -0400

    activity controller update
    update to the activity view controller

--- lab_6/lab-insta-parse/ActivityViewController.swift
+++ lab_6/lab-insta-parse/ActivityViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class ActivityViewController: UIViewController {
 
+    
     @IBOutlet weak var countryNameLabel: UILabel!
     @IBOutlet weak var countryDescriptionLabel: UILabel!
+
     @IBOutlet weak var activityButton1: UIButton!
     @IBOutlet weak var activityButton2: UIButton!
     @IBOutlet weak var activityButton3: UIButton!
@@ -19,34 +21,57 @@ class ActivityViewController: UIViewController {
     var selectedCountry: String?
     var countryDescription: String?
     var activities: [Activity]?
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
 
-        // Set up UI with data
-        countryNameLabel.text = selectedCountry
-        countryDescriptionLabel.text = countryDescription
+            // Set up UI with data
+            countryNameLabel.text = selectedCountry
+            countryDescriptionLabel.text = countryDescription
 
-        // Populate activity buttons
-        if let activities = activities {
-            for (index, activity) in activities.enumerated() {
-                switch index {
-                case 0:
-                    activityButton1.setTitle(activity.activityName, for: .normal)
-                    // Set background image for button if needed
-                    activityButton1.setBackgroundImage(UIImage(named: activity.imageName), for: .normal)
-                case 1:
-                    activityButton2.setTitle(activity.activityName, for: .normal)
-                    // Set background image for button if needed
-                    activityButton2.setBackgroundImage(UIImage(named: activity.imageName), for: .normal)
-                case 2:
-                    activityButton3.setTitle(activity.activityName, for: .normal)
-                    // Set background image for button if needed
-                    activityButton3.setBackgroundImage(UIImage(named: activity.imageName), for: .normal)
-                default:
-                    break
+            // Populate activity buttons
+            if let activities = activities {
+                for (index, activity) in activities.enumerated() {
+                    switch index {
+                    case 0:
+                        activityButton1.setTitle(activity.activityName, for: .normal)
+                    case 1:
+                        activityButton2.setTitle(activity.activityName, for: .normal)
+                    case 2:
+                        activityButton3.setTitle(activity.activityName, for: .normal)
+                    default:
+                        break
+                    }
                 }
             }
         }
+
+        // Action method for activity button 1
+        @IBAction func activityButton1Tapped(_ sender: UIButton) {
+            navigateToActivityDetail(for: 0)
+        }
+
+        // Action method for activity button 2
+        @IBAction func activityButton2Tapped(_ sender: UIButton) {
+            navigateToActivityDetail(for: 1)
+        }
+
+        // Action method for activity button 3
+        @IBAction func activityButton3Tapped(_ sender: UIButton) {
+            navigateToActivityDetail(for: 2)
+        }
+
+        private func navigateToActivityDetail(for index: Int) {
+            guard let activities = activities, index < activities.count else {
+                return
+            }
+            let selectedActivity = activities[index]
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let activityDetailVC = storyboard.instantiateViewController(withIdentifier: "ActivityDetailViewController") as? ActivityDetailViewController {
+                activityDetailVC.activityName = selectedActivity.activityName
+                activityDetailVC.activityDescription = selectedActivity.activityDescription
+                // Pass any other necessary data
+                navigationController?.pushViewController(activityDetailVC, animated: true)
+            }
+        }
     }
-}
